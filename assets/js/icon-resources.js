f36da57e54a8895841c18c86d4a4e08bece1374d
yogskr <yogakrisanta@gmail.com>
2024-01-29 00:09:34 +0700

    Added a module file

---
+++ assets/js/icon-resources.js
@@ -0,0 +1,19 @@
+const iconResources = {
+  thunderstorm: 'assets/images/weather-icons/thunderstorms-extreme.svg',
+  drizzle: 'assets/images/weather-icons/drizzle.svg',
+  rain: 'assets/images/weather-icons/rain.svg',
+  snow: 'assets/images/weather-icons/snow.svg',
+  mist: 'assets/images/weather-icons/mist.svg',
+  smoke: 'assets/images/weather-icons/smoke.svg',
+  haze: 'assets/images/weather-icons/haze.svg',
+  dust: 'assets/images/weather-icons/dust.svg',
+  fog: 'assets/images/weather-icons/fog.svg',
+  sand: 'assets/images/weather-icons/dust.svg',
+  ash: 'assets/images/weather-icons/dust.svg',
+  squalls: 'assets/images/weather-icons/hurricane.svg',
+  tornado: 'assets/images/weather-icons/tornado.svg',
+  clear: 'assets/images/weather-icons/clear.svg',
+  clouds: 'assets/images/weather-icons/clouds.svg',
+};
+
+export default iconResources;
